45df1dec88910050b1d774d3b864cc376e866111
Nitin Prakash <prakash.nitin63@gmail.com>
2022-08-23 00:20:14 +0530

    notes

--- src/TypeFamilies.hs
+++ src/TypeFamilies.hs
@@ -16,6 +16,8 @@ import Data.Kind (Type)
 import Data.Functor.Identity (Identity)
 import GHC.TypeLits (Symbol, KnownSymbol (..), symbolVal)
 import Data.Proxy ( Proxy (..) )
+import Data.Aeson (Value (..))
+import Data.Maybe (fromMaybe)
 
 --------------------------------------------------
 -- CLOSED TYPE FAMILIES
@@ -71,11 +73,22 @@ hlength (_ :& xs) = 1 + hlength xs
 -- OPEN TYPE FAMILIES
 --------------------------------------------------
 
+-- type Label :: Type -> Symbol
+-- type family Label t where
+--     Label Double = "number"
+--     Label String = "string"
+--     Label Bool   = "bool"
+
+-- the above is closed and obstructs the user from
+-- decalring their own custom type(possible in another module)
+-- we can enable that ommitting the @where@ keyword and making
+-- it an open type family
 type Label :: Type -> Symbol
-type family Label t where
-    Label Double = "number"
-    Label String = "string"
-    Label Bool   = "bool"
+type family Label t
+
+type instance Label Double = "number"
+type instance Label String = "string"
+type instance Label Bool = "bool"
 
 -- >>> label @Double
 -- "number"
@@ -83,3 +96,21 @@ type family Label t where
 -- "bool"
 label :: forall t. KnownSymbol (Label t) => String
 label = symbolVal (Proxy @(Label t))
+
+data MyType = MT
+type instance Label MyType = "mt"
+
+-- >>> label @MyType
+-- "mt"
+
+-- Open type families seem much more extensible
+-- but it comes at a cost
+-- the equations of an open type family are not allowed
+-- to overlap. But overlapping equations are often useful!
+
+-- Open type family instances must be compatible.
+-- Type family instances are compatible if at least
+-- one of the following holds:
+--
+-- Their left-hand sides are apart (i.e. not overlapping)
+-- Their left-hand sides unify with a substitution, under which the right-hand sides are equal.
