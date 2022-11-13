a72c6945a12355eb0dd16c5214c3de0b72ca02e0
Nitin Prakash <prakash.nitin63@gmail.com>
2022-11-13 14:30:36 +0530

    finish saddleback

--- src/Pearls/Saddleback.hs
+++ src/Pearls/Saddleback.hs
@@ -3,6 +3,10 @@
 -- of all pairs (x, y) satisfying @f (x , y) = z@. You can assume that f is strictly
 -- increasing in each argument, but nothing else.
 
+-- TODO: visit this chapter a few more times
+
+-- Reference: www.cs.geneseo.edu/~baldwin/math-thinking/saddleback.html
+
 
 {-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -44,7 +48,7 @@ invertByTheo f z =
 --
 -- >>> invertByAnne (+) 4
 -- [(0,4),(1,3),(2,2),(3,1),(4,0)]
-invertByAnne :: forall a. (Enum a, Eq a, Num a, Ord a) => (a -> a -> a) -> a -> [(a, a)]
+invertByAnne :: forall a. Integral a => (a -> a -> a) -> a -> [(a, a)]
 invertByAnne f z =
     -- find (0, z) f z
     find' (0, z) f z
@@ -52,12 +56,12 @@ invertByAnne f z =
       find :: (a, a) -> (a -> a -> a) -> a -> [(a, a)]
       find (u, v) f z = [(x, y) | x <- [u .. z], y <- [v, v - 1..0], f x y == z]
 
-      -- let's further improve ipon find defind above
-      -- proceeds directly to either the bottom or
-      -- rightmost boundary, it requires only z + 1 evaluations.
-      --In the worst case, traverses the perimeter of the square from the
+      -- let's further improve upon @find@ defind above
+      -- In the worst case, traverses the perimeter of the square from the
       -- top-left corner to the bottom-right corner, it performs 2z + 1 evaluations
-      -- of f
+      -- of f.
+      -- In the best case, when find proceeds directly to either the bottom or
+       -- rightmost boundary, it requires only z + 1 evaluations.
       find' :: (a, a) -> (a -> a -> a) -> a -> [(a, a)]
       find' (u, v) f z
           | u > z || v < 0 = []
@@ -66,3 +70,40 @@ invertByAnne f z =
           | z' > z = find' (u, v - 1) f z
           where
             z' = f u v
+
+-- What if we apply binary search?
+-- m = maximum (filter (\y → f (0, y) <= z ) [0 .. z])
+-- n = maximum (filter (\x -> f (x, 9) <= z) [0 .. z])
+--
+-- Then we can define invert f z = find (0, m) f z
+-- This way, rather than searching a (z+1) x (z+1) square we can get away
+-- with searching an (m+1) × (n+1) rectangle.
+--
+-- The reason the above works in because we're already given that
+-- top left corner is (0, z) and bottom right is (z, 0).
+-- We take advantage of this constraint.
+
+-- >>> invertImprovedByTheo (+) 3
+-- [(0,3),(1,2),(2,1),(3,0)]
+invertImprovedByTheo :: forall a. Integral a => (a -> a -> a) -> a -> [(a, a)]
+invertImprovedByTheo f z = find (0, m) f z
+  where
+    m = bsearch (\y -> f 0 y) (-1, z + 1) z
+
+    bsearch :: (a -> a) -> (a, a) -> a -> a
+    bsearch g (a, b) z
+        | a + 1 == b = a
+        | g m <= z   = bsearch g (m, b) z
+        | otherwise  = bsearch g (a, m) z
+        where
+          m = (a + b) `div` 2
+
+    -- exactly the same form invertByAnne, except that the first guard becomes (u > n || v < 0)
+    find (u, v) f z
+        | u > n || n < 0   = []
+        | z' < z           = find (u + 1, v) f z
+        | z' == z          = (u, v) : find (u + 1, v - 1) f z
+        | z' > z           = find (u, v - 1) f z
+        where
+          z' = f u v
+          n = maximum (filter (\x -> f x 0 <= z) [0 .. z ])
---
+++ src/Pearls/Selection.hs
@@ -0,0 +1,9 @@
+-- Chapter 4
+-- Objective: Let X and Y be two finite disjoint sets of elements over some ordered type
+-- and of combined size greater than k . Consider the problem of computing
+-- the k th smallest element of X ∪ Y . By definition, the k th smallest ele-
+-- ment of a set is one for which there are exactly k elements smaller than it,
+-- so the zeroth smallest is the smallest. How long does such a computation
+-- take?
+
+module Pearls.Selection where
